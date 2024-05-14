3c6e66fd270cdb97e4133369f147678e0dfc71d6
Daniel Whalin <danielj.whalin@gmail.com>
2024-05-13 23:16:44 -0700

    late night commit

--- models/campground.js
+++ models/campground.js
@@ -17,4 +17,14 @@ const CampgroundSchema = new Schema({
     ]
 });
 
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if(doc){
+        await review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
+
 module.exports = mongoose.model('Campground', CampgroundSchema);
