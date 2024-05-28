757e7cc58710f178e825e47e4c3b4b4718d1d961
Daniel Whalin <danielj.whalin@gmail.com>
2024-05-28 09:03:49 -0700

    Basic Authorization

--- middleware.js
+++ middleware.js
@@ -1,8 +1,18 @@
+
 module.exports.isLoggedIn = (req, res, next) => {
-    console.log("REQ.USER...", req.user);
     if (!req.isAuthenticated()) {
-        req.flash('error', 'you must be signed in first!');
+        req.session.returnTo = req.originalUrl; // add this line
+        req.flash('error', 'You must be signed in first!');
         return res.redirect('/login');
     }
     next();
 }
+
+
+module.exports.storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
+        res.locals.returnTo = req.session.returnTo;
+    }
+    next();
+}
+
