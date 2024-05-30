eae1117240df23e0e518912402bcd683d24b50bf
Daniel Whalin <danielj.whalin@gmail.com>
2024-05-30 09:27:50 -0700

    fixed "Review not defined" error

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
-const review = require('./models/review');
+const Review = require('./models/review');
 
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
@@ -52,7 +52,7 @@ module.exports.validateReview = (req, res, next) => {
 
 module.exports.isReviewAuthor = async (req, res, next) => {
     const { id, reviewId } = req.params;
-    const review = await Campground.findById(reviewId);
+    const review = await Review.findById(reviewId);
     if (!review.author.equals(req.user._id)) {
         req.flash('error', 'You do not have permission to do that!');
         return res.redirect(`/campgrounds/${id}`);
