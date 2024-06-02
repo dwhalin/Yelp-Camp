bdfc96a250790da9d3c5c0c3eb7c84eb23cc8de8
Daniel Whalin <danielj.whalin@gmail.com>
2024-06-02 10:24:22 -0700

    1st cloudinary commit

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== "prodcution") {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
