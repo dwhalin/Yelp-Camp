e1df1ab85dfb69edddacaaf1c6149e3af136c80c
Daniel Whalin <danielj.whalin@gmail.com>
2024-06-11 09:46:16 -0700

    test

--- app.js
+++ app.js
@@ -22,16 +22,11 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoStore = require('connect-mongo');
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
 
 //const localhost = ('127.0.0.1');
 //`mongodb://${localhost}:27017/yelp-camp`
-mongoose.connect(dbUrl, {
-    //useNewUrlParser: true,
-    //useCreateIndex: true,
-    //useUnifiedTopology: true,
-    //useFindAndModify: false
-});
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection:"));
